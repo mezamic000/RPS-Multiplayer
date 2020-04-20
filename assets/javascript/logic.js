0fabca7e57a3e10ad891a1efe5138c82b30fead1
Firecraker <mezamic000@gmail.com>
2020-04-20 13:50:09 -0500

    added start menu with user input and button

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,19 +1,66 @@
+// Initialize Firebase
+var config = {
+	apiKey: "AIzaSyCkLG54wYFrjtH29Sv_6tUIHOnPogOAesk",
+	authDomain: "rock-c8d5c.firebaseapp.com",
+	databaseURL: "https://rock-c8d5c.firebaseio.com",
+	projectId: "rock-c8d5c",
+	storageBucket: "rock-c8d5c.appspot.com",
+};
+
+firebase.initializeApp(config);
+
+var dataRef = firebase.database();
+
+function createUserName() {
+	//code here
+}
+
 var choices = ["rock", "paper", "scissors"];
 var player1WinScore = 0;
 var player2WinScore = 0;
 var player1LoseScore = 0;
 var player2LoseScore = 0;
 
-const player1WinScore_span = document.getElementById("player1-score");
-const player2WinScore_span = document.getElementById("player2-score");
-const player1Score_span = document.getElementById("player1-score");
-const player2Score_span = document.getElementById("player2-score");
+function startMenu() {
+	$("#start-menu").show();
+	$("#start-image").show();
+	$(".score-board").hide();
+	$(".choices").hide();
+	$("#action-message").hide();
+}
+startMenu();
 
-const scoreBoard_div = document.querySelector(".score-board");
-const result_div = document.querySelector(".result");
-const rock_button = document.getElementById("rock");
-const paper_button = document.getElementById("paper");
-const scissors_button = document.getElementById("scissors");
+document.getElementById("start-button").addEventListener("click", function () {
+	$("#start-menu").hide(); //change line the next 5 lines to Vanilla Javascript
+	$("#start-image").hide();
+	$(".score-board").show();
+	$(".choices").show();
+	$("#action-message").show();
+	main();
+});
+
+function main() {
+	document.getElementById("action-message").innerHTML =
+		"Select Rock, Paper, or Scissors";
+	document.getElementById("player1-WinScore").innerHTML = player1WinScore;
+	document.getElementById("player2-LoseScore").innerHTML = player2LoseScore;
+	document.getElementById("player2-WinScore").innerHTML = player1WinScore;
+	document.getElementById("player1-LoseScore").innerHTML = player2LoseScore;
+
+	document.getElementById("rock").addEventListener("click", function () {
+		document.getElementById("action-message").style.display = "none";
+		game("rock");
+	});
+	document.getElementById("paper").addEventListener("click", function () {
+		document.getElementById("action-message").style.display = "none";
+		game("paper");
+	});
+
+	document.getElementById("scissors").addEventListener("click", function () {
+		document.getElementById("action-message").style.display = "none";
+		game("scissors");
+	});
+}
 
 function getPlayer2Choice() {
 	var randomNumber = Math.floor(Math.random() * 3);
@@ -25,6 +72,7 @@ function win(player1Choice, player2Choice) {
 	//move data to firebase
 	player1WinScore++;
 	player2LoseScore++;
+
 	document.getElementById("player1-WinScore").innerHTML = player1WinScore;
 	document.getElementById("player2-LoseScore").innerHTML = player2LoseScore;
 	document.querySelector(".result").style.display = "block";
@@ -46,8 +94,8 @@ function lose(player1Choice, player2Choice) {
 	//move data to firebase
 	player1LoseScore++;
 	player2WinScore++;
-	document.getElementById("player1-LoseScore").innerHTML = player1WinScore;
-	document.getElementById("player2-WinScore").innerHTML = player2LoseScore;
+	document.getElementById("player1-LoseScore").innerHTML = player1LoseScore;
+	document.getElementById("player2-WinScore").innerHTML = player2WinScore;
 	document.querySelector(".result").style.display = "block";
 	document.querySelector(".result").innerHTML = (
 		player2Choice +
@@ -99,21 +147,3 @@ function game(player1Choice) {
 			break;
 	}
 }
-
-function main() {
-	document.getElementById("action-message").innerHTML =
-		"Select Rock, Paper, or Scissors";
-
-	document.getElementById("rock").addEventListener("click", function () {
-		document.getElementById("action-message").style.display = "none";
-		game("rock");
-	});
-	document.getElementById("paper").addEventListener("click", function () {
-		game("paper");
-	});
-
-	document.getElementById("scissors").addEventListener("click", function () {
-		game("scissors");
-	});
-}
-main();
